646ff1f2cf7305b1386f69cdbfaf11c115ebffeb
grantquo <grantgutterman@gmail.com>
2024-02-18 21:21:59 -0500

    creation of input files and function building

--- program2/primegen.py
+++ program2/primegen.py
@@ -1,22 +1,29 @@
-#main function
+# main function
 
 def prime(starting_range, ending_range):
     prime_storage = []
     flag = 0
-    for increment1 in range(starting_range, ending_range):
-        for increment2 in range(2, increment1):
-            if (increment1%increment2 == 0):
-                flag = 1
-                break
-            else:
-                flag = 0
-        if (flag == 0):
-            prime_storage.append(increment1)
+    stopper = 0
+    increment1 = starting_range
+    while increment1 < ending_range and stopper == 0:
+        if len(prime_storage) < 10:
+            print(f"WHILE RUN: {increment1}")
+            for increment2 in range(2, increment1):
+                if (increment1%increment2 == 0):
+                    flag = 1
+                    break
+                else:
+                    flag = 0
+                if (flag == 0):
+                    prime_storage.append(increment1)
+            increment1 += 1
+        else:
+            stopper = 1
     return prime_storage
 
 #driver
 
-# starting_range = 0
-# ending_range = 1000
+# starting_range = 4294967296 
+# ending_range = 4500000000
 # prime_storage = prime(starting_range, ending_range)
 # print(prime_storage)
--- program2/rsa.py
+++ program2/rsa.py
@@ -7,32 +7,56 @@ or decryption, processing them through RSA and outputting the result.
 """
 import sys
 import os
-
-
-os.chdir("/home/kingkoobie/cs401/program2")
+import primegen
 
 #Take Input
-with open("input1.txt","r") as f:
+os.chdir("/home/kingkoobie/cs401/program2")
+with open("input1text.txt","r") as f:
     startingtext = f.read()
 
-
 #Present Input String
-def get_input(text):
-    text_char = ""
-    data_list = text.split()
+def get_input(txt):
+    txt_char = ""
+    data_list = txt.split()
     for iii in data_list:
         print(iii)
         int_iii = int(iii)
-        text_char += chr(int_iii)
-    return print(text_char)
+        txt_char += chr(int_iii)
+    return print(txt_char)
 
-
-def block_splitter():
+def block_splitter(block_num):
     pass
+
+
+#Encryption
+def public_key(ppp, qqq, eee):
+    pass
+
+def private_key():
+    pass
+
+def encryption():
+    pass
+
+#Global RSA Vars
+ppp = 0
+qqq = 0
+block_size = 0
 
 def main():
-    pass
+    f = open("input1pq.txt","r")
+    pqinputs = f.readline()
 
-print(startingtext)
-get_input(startingtext)
+
+
+
+    getpq(pqinputs, ppp, qqq, block_size)
+    print(f"ppp: {ppp}\nqqq: {qqq}\nblock_size: {block_size}")
+    f.close()
+    return
+    
+
+#Procedures
+
+
 main()
